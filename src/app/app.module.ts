5c4aed963738edf554414c9d0f1314461c296f37
Ikhalas  Mannoon <5910110405@psu.ac.th>
2019-10-15 03:48:12 +0700

    Ch 6 AddAction AddEffect AddReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NgZone, CUSTOM_ELEMENTS_SCHEMA, PLATFORM_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -12,6 +12,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from 'src/environments/environment';
 import { TopStoriesModule } from './top-stories/top-stories.module';
+import { reducers } from './reducers';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ItemsEffects } from './effects/items';
+import { HACKER_NEWS_DB } from './hackernews-db';
+import { ServicesModule } from './services/services.module';
 
 @NgModule({
   declarations: [AppComponent],
